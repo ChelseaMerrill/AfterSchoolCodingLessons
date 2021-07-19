4400bca6c8d4a0896f3b68a107c8793191711a79
Chelsea Merrill <chelmer1223@aol.com>
2021-07-19 16:34:41 -0400

    notes ready for workshop

---
+++ notes.js
@@ -0,0 +1,42 @@
+//KEY ELEMENTS
+
+//HTML = HyperText Markup Language:
+        //the standard markup language for documents designed to be displayed in a web browser. 
+        //It can be assisted by technologies such as CSS and scripting languages (JavaScript).
+        //structure
+//CSS = Cascading Style Sheets:
+        // describes the presentation of the document
+        //style
+
+//Script Language = JavaScript(for this example)
+        // gives functionality to the website
+        //allows us to make the elements on the page operate    
+
+
+//SKELETON:
+
+// <!DOCTYPE html>
+// <html>
+//     <head>
+//         <title>Title</title>
+//         <link rel='stylesheet' href="styles.css"/>
+//     </head>
+//     <body>
+//       
+//      <p> content </p>  
+//        
+//      <script src="script.js"></script>
+//     </body>
+// </html>
+
+
+//HTML Elements:
+    //  start tag, content, end tag
+        // <h1>Heading1</h1>, <p>paragraph</p>, <br/>
+    //  <div> tags
+    // class names (.)
+    // id (#)
+
+//SCRIPT 
+    // adding event listeners: attaches an event handler to the specified element. You can add many event handlers to one element
+    //Query Selector v Query Selector all
