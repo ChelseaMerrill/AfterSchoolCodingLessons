9c72b4d1797743a08d5327323340ee5896c3724d
Chelsea Merrill <chelmer1223@aol.com>
2021-08-16 18:56:13 -0400

    8/16 After School Coding Lesson

---
+++ .vscode/Notes/AfterSchoolCodingSession/Keyboard-Tayden/script.js
@@ -0,0 +1,5 @@
+const screen = document.querySelector('#screen');
+const button = document.querySelectorAll('.button')
+const letters = document.querySelectorAll('.letter');
+const space = document.querySelector('.space');
+const back = document.querySelector('.back');
