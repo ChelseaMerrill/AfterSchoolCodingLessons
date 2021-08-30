18c017e7cf3c757f085340f7bd353d0cff285664
Chelsea Merrill <chelmer1223@aol.com>
2021-08-30 17:50:41 -0400

    lesson taught

--- AfterSchoolCodingSession/Notes/notes.js
+++ AfterSchoolCodingSession/Notes/notes.js
@@ -1,4 +1,4 @@
-//KEY ELEMENTS
+//KEY ELEMENTS 8/16/2021
 
 //HTML = HyperText Markup Language:
         //the standard markup language for documents designed to be displayed in a web browser. 
@@ -42,7 +42,7 @@
     //Query Selector v Query Selector all
 
 /* -------------------------------------------------------------------------- */
-// VARIABLE NOTES
+// VARIABLE NOTES 8/23/2021
 
 // What are variables?
 // containers for storing data values
@@ -132,7 +132,8 @@
 
 // console.log(typeof 6) // 'number'
 // console.log(typeof 'hi') // 'string' 
-// console.log(typeof {string: 'hi'}) // 'object'
+// console.log(typeof {string: 'hi'}) 
+// 'object'
 // console.log(typeof [1, 2, 3]) // 'object'
 
 // FUNCTION NOTES
@@ -222,5 +223,180 @@
 // console.log(iterateArrays(stringArray))
 
 /* -------------------------------------------------------------------------- */
+ 
+// NUMBER NOTES 8/30/2021
+
+// Number Definition:
+//  JS numbers are the same as numbers we traditionally use in math 
+
+/* -------------------------------------------------------------------------- */
+
+// // Add (+)
+console.log(5+5)
+//10
+
+// // Substract (-)
+console.log(10-2)
+//8
+
+// // Divide (/)
+console.log(60/3)
+//20
+
+// // Multiply (*)
+console.log(3 * 7)
+//21
+
+// // Compare: <, >, >=, <=
+console.log(4 > 7) 
+//false
+console.log(5 < 15) 
+//true
+console.log(7 >= 7) 
+//true
+console.log(8 <= 9 ) 
+//true
+
+/* -------------------------------------------------------------------------- */
+
+// Modular % definition: operator (%) returns the division remainder
+
+console.log(12 % 5)
+//2
+
+console.log(15 % 3) 
+//0
+
+console.log(10 % 3)
+// 
+
+/* -------------------------------------------------------------------------- */
+
+// MATH METHODS
+
+// Math.sqrt() definition: Return the square root of a number
+
+console.log(Math.sqrt(8)) 
+//2.8284271247461903
+
+console.log(Math.sqrt(16))
+//4
+
+console.log(Math.sqrt(25)) 
+//5
+
+/* -------------------------------------------------------------------------- */
+
+// Math.pow(base, exponent) 
+
+console.log(Math.pow(2, 3))
+//8
+
+console.log(Math.pow(7, 20)) 
+//79792266297612000
+
+/* -------------------------------------------------------------------------- */
+
+// Math.round() definition: returns the value of a number rounded to the nearest integer
+
+console.log(Math.round(3.14)) 
+//3
+
+console.log(87/15) 
+console.log(Math.round(87/15)) 
+//6
+
+// console.log(Math.round(1.5)) 
+
+/* -------------------------------------------------------------------------- */
+
+// Math.ceil() definition: rounding up no matter what
+
+console.log(Math.ceil(1.1)) 
+//2
+
+// console.log(Math.ceil(19.01247502)) 
+
+/* -------------------------------------------------------------------------- */
+
+// Math.floor() definition: rounding down no matter what 
+
+console.log(Math.floor(3.14))
+//3
+
+// console.log(Math.floor(15/7))
+// console.log(Math.floor(3.9)) 
+
+
+let num = Math.floor(15/7)
+let remainder = 15 % 7
+
+console.log('15/7 is ' + num + ' with a remainder of ' + remainder ) 
+// 15/7 is 2 with a remainder of 1
+
+/* -------------------------------------------------------------------------- */
+
+// Math.abs() definition: returns the absolute number. Will return positive numbers only 
+
+// console.log(Math.abs(15-6)) 
+// console.log(6-15) 
+console.log(Math.abs(6-15))
+//9
+
+/* -------------------------------------------------------------------------- */
+
+// Math.min() difinition: returns the minimum number 
+
+console.log(Math.min(7, 20, 4, 1, 25, 7, 3, 3.14, 24, -1, -100, -7))
+//-100
+
+/* -------------------------------------------------------------------------- */
+
+// Math.max() definition: returns the maximum number 
+
+console.log(Math.max(7, 20, 4, 1, 25, 7, 3, 3.14, 24, -1, -100, -7)) 
+//25
+
+/* -------------------------------------------------------------------------- */
+
+// Math.random()
+
+// console.log(Math.random()) //random numbers between 0-1 in decimals 
+// console.log(Math.random() * 100) // random numbers between 0-100 in decimals 
+// console.log(Math.floor(Math.random() * 100)) //random whole numbers between 0-100 rounded down to the nearest whole number
+// console.log(Math.ceil(Math.random() * 100)) //random whole numbers between 0-100 rounded up to the nearest whole number
+
+
+/* -------------------------------------------------------------------------- */
+
+// Increasing and decreasing variables
+
+let i = 7
+// console.log(i)
+//7 
+
+i = i + 1
+// console.log(i) 
+//8
+
+i += 1
+// console.log(i)
+//9
+
+i++
+// console.log(i) 
+//10
+
+// i = i - 1
+// console.log(i) 
+
+// i -= 1
+// console.log(i) 
+
+// i--
+// console.log(i) 
+
+
+
 /* -------------------------------------------------------------------------- */
 
